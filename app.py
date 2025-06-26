228e07ef50140407f565fe9d77c32380947ecdec
unknown <rajvivachhani06@gmail.com>
2025-06-26 11:13:51 +0530

    Fix runtime and db init for Render

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
+with app.app_context():
     db.create_all()
 
 @app.route('/')
